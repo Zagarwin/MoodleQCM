aa0e7ec280cc16797fbebed5b4830980a11be520
llouis3 <35458097+llouis3@users.noreply.github.com>
2019-01-03 16:31:14 +0100

    Ajout Controller

--- MoodleQCM/src/sample/Controller.java
+++ MoodleQCM/src/sample/Controller.java
@@ -1,8 +1,32 @@
 package sample;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.Label;
+import javafx.fxml.FXML;
+import javafx.scene.control.TreeView;
 
 public class Controller {
 
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TreeView<?> tree;
+
+    @FXML
+    void treeDrag(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert tree != null : "fx:id=\"tree\" was not injected: check your FXML file 'sample.fxml'.";
+
+    }
 }
+
+
