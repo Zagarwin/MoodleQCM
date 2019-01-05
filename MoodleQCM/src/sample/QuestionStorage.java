7f0abd67c8f9ee162dd7f19069ccc95b26264fa5
Yufei6 <m.liuyufei@qq.com>
2019-01-05 17:26:46 +0100

    Add QuestionStorage

--- MoodleQCM/src/sample/QuestionStorage.java
+++ MoodleQCM/src/sample/QuestionStorage.java
@@ -1,18 +1,18 @@
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.StringReader;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import java.util.HashSet;
+import java.util.Set;
+
 
 
 class QuestionStorage{
@@ -23,28 +23,13 @@ class QuestionStorage{
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         try {
             DocumentBuilder builder = factory.newDocumentBuilder();
-            StringReader sr = new StringReader(strXML);
-            InputSource is = new InputSource(sr);
-            Document doc = builder.parse(is);
-            Element rootElement = doc.getDocumentElement();
-            NodeList phones = rootElement.getElementsByTagName("type");
-            for (int i = 0; i < phones.getLength(); i++) {
-                Node type = phones.item(i);
-                String id_string = ((Element)type).getAttribute("id");
-                int id_int = (int)id_string;
-                listeIDs.add(id_int);
-                NodeList properties = type.getChildNodes();
-                // for (int j = 0; j < properties.getLength(); j++) {
-                //     Node property = properties.item(j);
-                //     String nodeName = property.getNodeName();
-                //     if (nodeName.equals("price")) {
-                //         String price=property.getFirstChild().getNodeValue();
-                //         System.out.println("price="+price);
-                //     } else if (nodeName.equals("operator")) {
-                //         String operator=property.getFirstChild().getNodeValue();
-                //         System.out.println("operator="+operator);
-                //     }
-                // }
+            Document document= builder.parse(new File(strXML));
+            Element racine = document.getDocumentElement();
+            final NodeList list_Id = racine.getElementsByTagName("question_id_list");
+            final int nbIDsElements = list_Id.getLength();
+            for(int i = 0; i<nbIDsElements; i++) {
+                final Element Id = (Element) list_Id.item(i);
+                listeIDs.add(Integer.parseInt(Id.getTextContent()));
             }
         } catch (ParserConfigurationException e) {
             // TODO Auto-generated catch block
@@ -58,14 +43,4 @@ class QuestionStorage{
         }
     }
 
-
-    public void quizz(){
-        
-    }
-
-    public void bank(){
-        
-    }
-
 }
-
